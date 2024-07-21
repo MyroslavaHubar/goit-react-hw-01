2f578de8f25bd855fd26fce3a193c0ea6577bb3c
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-07-21 22:51:46 +0200

    add new folder FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,24 +1,21 @@
-import FriendListItem from "./FriendListItem";
-
+import FriendListItem from "../FriendListItem/FriendListItem";
 
 function FriendList({ friends }) {
-    return (
-        <ul>
-            {friends.map(friend => {
-                return (
-                    <li key={friend.id}>
-                        <FriendListItem
-                            avatar={friend.avatar}
-                            name={friend.name}
-                            isOnline={friend.isOnline}
-                        />
-                    </li>
-                );
-            })}
-	
-        </ul>
-
-    );
+  return (
+    <ul>
+      {friends.map((friend) => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default FriendList;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,31 @@
-import css from './Profile.module.css'
+import css from "./Profile.module.css";
 
 function Profile({ name, tag, location, image, stats }) {
-    return (
-        <div className={css.profileSection}>
-            <div className={css.profileCard}>
-                <img className={css.avatar}
-                    src={image}
-                    alt="User avatar"
-                />
-                <p className={css.profileName}>{name}</p>
-                <p className={css.profileTag}>@{tag}</p>
-                <p className={css.profileLocation}>{location}</p>
-            </div>
+  return (
+    <div className={css.card}>
+      <div className={css.profile}>
+        <img className={css.avatar} src={image} alt="User avatar" />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
 
-            <ul className={css.stats}>
-                <li>
-                    <span>Followers</span>
-                    <span className={css.statsValue}>{stats.followers}</span>
-                </li>
-                <li>
-                    <span>Views</span>
-                    <span className={css.statsValue}>{stats.views}</span>
-                </li>
-                <li>
-                    <span>Likes</span>
-                    <span className={css.statsValue}>{stats.likes}</span>
-                </li>
-            </ul>
-        </div>
-
-    );
+      <ul className={css.stats}>
+        <li>
+          <span>Followers</span>
+          <span className={css.statsValue}>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span className={css.statsValue}>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span className={css.statsValue}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Profile;
